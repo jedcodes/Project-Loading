08739d4ab1b37f79ba0cb1f60ff21bf289c5192b
Jack Delamou <jackdelamou.97@gmail.com>
2024-05-29 15:18:48 +0200

    Test commit

--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold text-teal-400">Hello Reactdfdf!!!</h1>
+      <h1 className="text-3xl font-bold text-teal-400">Hello Reactdfdf</h1>
     </>
   );
 }
