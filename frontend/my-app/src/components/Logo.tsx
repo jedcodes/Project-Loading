186921c0064c92d1ca1854bd2fe363933a3d5fb9
Jack Delamou <jackdelamou.97@gmail.com>
2024-06-04 20:52:22 +0200

    Delete
    Deleted frontend mappa

--- frontend/my-app/src/components/Logo.tsx
+++
@@ -1,9 +0,0 @@
-const Logo = () => {
-  return (
-    <h1 className="bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text text-7xl font-bold tracking-wider m-5">
-      LOADING
-    </h1>
-  );
-};
-
-export default Logo;
