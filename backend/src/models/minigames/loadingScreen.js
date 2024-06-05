35b2b3071ea4b14b06adec4db66ffbb111268bd4
nisjety <ima_stovers24@hotmail.com>
2024-06-06 01:04:41 +0200

    added loadingScreen.js

--- backend/src/models/minigames/loadingScreen.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const loadingScreenSchema = new mongoose.Schema({
-    message: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now }
-});
-
-const LoadingScreen = mongoose.model('LoadingScreen', loadingScreenSchema);
-export { LoadingScreen };
